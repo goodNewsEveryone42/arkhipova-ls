60c6630ee4b815f8bee0bf4173050dcdc088f881
Anna Arkhipova <qywy1234@gmail.com>
2020-06-02 21:16:55 +1000

    update slider review

--- src/scripts/review.js
+++ src/scripts/review.js
@@ -1,21 +1,18 @@
 import Vue from "vue";
+import { Carousel, Slide } from 'vue-carousel';
 
 
 const titleBtn = {
   template: "#review-title-btn"
 }
 
-const slide = {
-  template: "#review-slider-items",
-  props: ["slide", "slides", "currentIndex"]
-}
-
 const slider = {
   template: "#review-slider-container",
   components: {
-    slide
+    Slide,
+    Carousel
   },
-  props: ["slides", "currentIndex"]
+  props: ["slides"]
 }
 
 new Vue({
@@ -23,55 +20,17 @@ new Vue({
   template: "#review",
   components: {
     titleBtn,
-    slider
+    slider,
+    Carousel,
+    Slide
   },
 
   data() {
     return {
       slides: [],
-      currentIndex: 0
     }
   },
 
-  watch: {
-    currentIndex(value) {
-      this.makeInfiniteLoop(value);
-    }
-},
-
-  methods: {
-    handleClick(event) {
-      if (event==="next") {
-        let arrayMake = this.makeArraySlides();
-        let counter = this.currentIndex++;
-        console.log(counter);
-        for (let i = 0; i < arrayMake.length; i++) {
-          if (counter === i) {
-            arrayMake[counter].style.display = 'none';
-        }
-      }
-      } else if (event==="prev") {
-        this.currentIndex--;
-        console.log(this.currentIndex)
-      }
-    },
-
-    makeInfiniteLoop(value) {
-      let arrayMake = this.makeArraySlides();
-      const worksAmountZero = arrayMake.length - 1;
-        if (value > worksAmountZero) {
-            this.currentIndex = 0;
-        } else if (value < 0) {
-            this.currentIndex = worksAmountZero;
-        }
-    },
-
-    makeArraySlides() {
-      let arraySlides = this.$el.querySelectorAll('.slider-review__container');
-      return arraySlides;
-    }
-  },
-  
   created() {
     const data = require("../data/review.json");
     this.slides = data;
