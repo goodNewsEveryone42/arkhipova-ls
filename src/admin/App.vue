e95e8355aa9fb4e6d60079dca5023131697fad4c
Anna Arkhipova <qywy1234@gmail.com>
2020-07-28 15:32:28 +1000

    removing unnecessary items

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,32 +1,32 @@
 <template lang="pug">
-  .root
-    .root-container
-      router-view(name="headline")
-      router-view(name="tabs")
-    .container
-      main.content-container
-        router-view
-    router-view(name="login")
+.root
+  .root-container
+    router-view(name="headline")
+    router-view(name="tabs")
+  .container
+    main.content-container
+      router-view
+  router-view(name="login")
 </template>
 
 <script>
-  import headline from "./components/headline";
-  import tabs from "./components/tabs";
-  import login from "./components/login";
+import headline from "./components/headline";
+import tabs from "./components/tabs";
+import login from "./components/login";
 
-  export default {
-    components: {
-      headline,
-      tabs,
-    },
-    methods: {
-     handleChange(tab) {
+export default {
+  components: {
+    headline,
+    tabs,
+  },
+  methods: {
+    handleChange(tab) {
       this.activeTabId = tab.id;
-      this.$emit("tabChanged", tab);
+      // this.$emit("tabChanged", tab);
       this.$router.push(tab.href);
-    }
-  }
-  }
+    },
+  },
+};
 </script>
 
 <style lang="postcss">
--- src/admin/components/tabs/tabs.vue
+++ src/admin/components/tabs/tabs.vue
@@ -1,43 +1,40 @@
 <template lang="pug">
-  section.tabs-container
-    .container
-      .tabs__wrapper
-        ul.tabs
-          li.tabs__item(
-            v-for="tab in tabs" :key="tabs.id"
-            :class="{'tabs__link--active' : activeTabId === tab.id}"
-            )
-            a.tabs__link(
-              @click = "handleChange(tab)"
-            ) {{tab.title}}
+section.tabs-container
+  .container
+    .tabs__wrapper
+      ul.tabs
+        li.tabs__item(
+          v-for="tab in tabs",
+          :key="tabs.id",
+          :class="{ 'tabs__link--active': activeTabId === tab.id }"
+        )
+          a.tabs__link(@click="handleChange(tab)") {{ tab.title }}
 </template>
 
 <script>
-
 const tabs = [
-  {id: 0, title: "Обо мне", href: "/about"},
-  {id: 1, title: "Работы", href: "/works"},
-  {id: 2, title: "Отзывы", href: "/reviews"},
-]
+  { id: 0, title: "Обо мне", href: "/about" },
+  { id: 1, title: "Работы", href: "/works" },
+  { id: 2, title: "Отзывы", href: "/reviews" },
+];
 export default {
   data() {
     return {
       tabs,
-      activeTabId: 0
-    }
+      activeTabId: 0,
+    };
   },
   methods: {
     handleChange(tab) {
       this.activeTabId = tab.id;
-      this.$emit("tabChanged", tab);
+      // this.$emit("tabChanged", tab);
       this.$router.push(tab.href);
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="postcss" scoped>
-
 .tabs-container {
   background-color: #ffffff;
 }
@@ -66,7 +63,7 @@ export default {
     min-width: 320px;
     width: 100%;
   }
-  
+
   .tabs__link {
     line-height: 36px;
     display: block;
@@ -77,5 +74,4 @@ export default {
     white-space: nowrap;
   }
 }
-
 </style>
